ffed235c51099b683c6da4194efc6fe6ffe24cc3
oualidor <walid.khial@gmail.com>
2021-08-27 17:39:44 +0100

    test

--- socketServerIndex.js
+++ socketServerIndex.js
@@ -4,49 +4,21 @@ const net = require("net"); // import net
 // create the server
 let server = net.createServer(connection => {
     // run all of this when a client connects
-    console.log("new connection");
+    console.log("Station connected ");
 
     connection.on("data", data => {
         // run this when data is received
         if (data == undefined || data == null) {
-            return;
+            console.log("no data found")
         }
 
         const dataArgs = data.toString().split(" "); // splits the data into spaces
 
         if (dataArgs.length === 0) { // in case there is no command
-            connection.write("ERROR no data");
+            console.log("data length 0")
             return; // prevents other code from running
         }
-        const command = dataArgs[0]; // gets the command
-        if (command === "ADD") { // add command
-            if (dataArgs.length !== 3) { // in case there aren't enough arguments
-                connection.write("ERROR incorrect number of arguments");
-                return;
-            }
+        console.log(data)
 
-            const op1 = parseInt(dataArgs[1]); // first number
-            const op2 = parseInt(dataArgs[2]); // second number
-            const result = (op1 + op2).toString(); // result as a string
-
-            if (result === "NaN") { // in case the inputs aren't numbers
-                connection.write("ERROR invalid number");
-                return;
-            }
-
-            connection.write("RESULT " + result);
-            return; // end
-        } else { // invalid command
-            connection.write("ERROR invalid command");
-            return;
-        }
     });
-});
-
-
-// look for a connection on port 50,000
-server.listen(3001, "", () => {
-    let address = server.address();
-    console.log("server running on " + address)
-    console.log("waiting for a connection"); // prints on start
 });
