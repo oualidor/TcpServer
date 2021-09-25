ab6881b900f5d9efde83a38193e12ea41cae4b52
oualidor <walid.khial@gmail.com>
2021-09-25 10:08:57 +0100

    Test, dont fetch here

--- Apis/Config.js
+++ Apis/Config.js
@@ -1,9 +1,9 @@
 const TCP_PORT = 4000
 const EXPRESS_PORT = 3000
 
-const HOST = '164.4.59.129'
+const HOST = '164.132.59.129'
 //const BACKEND_SERVER = "http://localhost:8080/"
-const BACKEND_SERVER = "http://164.4.59.129:8080/"
+const BACKEND_SERVER = "http://164.132.59.129:8080"
 const adminMail = "walid.khial@gmail.com"
 const adminPassword  ="";
 const jwtPrivateKey = "lkjlfngrpgjefvml,s:;vnsomvfijv";
