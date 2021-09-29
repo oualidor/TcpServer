cd0aa9ed14a7341c9ef97b5908d20a2a045a6789
oualidor <walid.khial@gmail.com>
2021-09-29 11:24:52 +0100

    debugging dont fetch here

--- Structures/LoginRequest.js
+++ Structures/LoginRequest.js
@@ -5,8 +5,15 @@ const LoginRequest = (data) => {
 
     return ({
         length: parseInt(data.substr(0, 4), 16 *2),
+        cmd: data.substr(4, 2),
+        version: data.substr(6, 2),
+        checkSum: data.substr(8, 2),
+        token: data.substr(10, 8),
+        random: data.substr(18, 8),
+        magic: data.substr(26, 4),
         boxIdLength: parseInt(data.substr(30, 4), 16)*2,
-        boxId: Converter.hexToString(data.substr(32, this.boxIdLength)).replace(/[^a-zA-Z0-9]/g, "")
+        encryptedBoxId: data.substr(34, this.boxIdLength),
+        boxId: Converter.hexToString(data.substr(34, this.boxIdLength)).replace(/[^a-zA-Z0-9]/g, "")
     })
 
 };
