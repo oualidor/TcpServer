b0ad4c894d0523e60e93b86342db33befad7456b
oualidor <walid.khial@gmail.com>
2021-09-29 12:01:23 +0100

    debugging dont fetch here

--- Apis/ConnectionEvents.js
+++ Apis/ConnectionEvents.js
@@ -19,26 +19,32 @@ const ConnectionEvents = {
         connection.removeAllListeners("data")
         ConsoleMsgs.success("Setting data event to wait for query info only")
         connection.on("data", data => {
-            data = data.toString("hex")
-            let cmd = RequestOperations.CmdExtractor(data)
-            if (cmd != undefined) {
-                if (cmd == CMDs.PowerBankInfo) {
-                    connection.removeAllListeners("data")
-                    connection.on("data", data => {
-                        data = data.toString('hex')
-                        ConnectionEvents.General(clientsList, connection, data)
+            try {
+                data = data.toString("hex")
+                let cmd = RequestOperations.CmdExtractor(data)
+                if (cmd != undefined) {
+                    if (cmd == CMDs.PowerBankInfo) {
+                        connection.removeAllListeners("data")
+                        connection.on("data", data => {
+                            data = data.toString('hex')
+                            ConnectionEvents.General(clientsList, connection, data)
 
-                    })
-                    ConsoleMsgs.success("Query info caught, Setting data event to general and sending data to user")
-                    res.send({finalResult: true, data: PowerBankQueries.PowerBankQueryResult(data)})
-                } else {
-                    console.log("ignoring data cause waiting for query info only")
+                        })
+                        ConsoleMsgs.success("Query info caught, Setting data event to general and sending data to user")
+                        res.send({finalResult: true, data: PowerBankQueries.PowerBankQueryResult(data)})
+                    } else {
+                        console.log("ignoring data cause waiting for query info only")
+                    }
+                }else {
+                    ConsoleMsgs.error("Cmd is undefined, kicking out teh client")
+                    connection.terminate()
+                    res.send({finalResult: false, error: "Operation result in kicking gout teh client fro un allowed request"})
                 }
-            }else {
-                ConsoleMsgs.error("Cmd is undefined, kicking out teh client")
-                connection.terminate()
-                res.send({finalResult: false, error: "Operation result in kicking gout teh client fro un allowed request"})
+            }catch (error){
+                ConsoleMsgs.error(error)
+                res.send({finalResult: false, error: "Request failed due to intern error"})
             }
+
         })
     }
 }
