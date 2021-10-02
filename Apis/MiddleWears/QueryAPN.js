cb695e15092a8f01b99cd7375b4930fefef3d24f
oualidor <walid.khial@gmail.com>
2021-10-02 13:30:35 +0100

    debugging

--- Apis/MiddleWears/QueryAPN.js
+++ Apis/MiddleWears/QueryAPN.js
@@ -2,14 +2,21 @@ const {ExpressResult} = require("../../Structures/ExpressResult");
 const QueryAPN = {
     dataValidator: (req, res, next) => {
         let valid = true
+        let  error = ""
         let acceptedIndexes = ["00", "01", "02", "03", "04"]
         let {stationId, index} = req.params
-        if (acceptedIndexes.includes(index) == false) valid = false
-        if(stationId == null || stationId == undefined || stationId.length == 0 || stationId.length == undefined ) valid =false
+        if (acceptedIndexes.includes(index) == false){
+            valid = false
+            error = error + "/ wrong index"
+        }
+        if(stationId == null || stationId == undefined || stationId.length == 0 || stationId.length == undefined ){
+            valid =false
+            error = error + "/ wrong station ID"
+        }
         if(valid){
             next()
         }else {
-            res.send(ExpressResult(false, "wrong index"))
+            res.send(ExpressResult(false, error))
         }
     }
 }
