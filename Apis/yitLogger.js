5725447f334943b7ddb07ea2bcabe8a7794081e1
oualidor <walid.khial@gmail.com>
2021-10-05 18:52:06 +0100

    Log to directory

--- Apis/yitLogger.js
+++ Apis/yitLogger.js
@@ -3,7 +3,7 @@ const rt = require("file-stream-rotator")
 
 morgan.token("custom", "(:method) :url => :status :remote-addr" )
 
-let logWriter = rt.getStream({filename:"log.log", frequency:"daily", verbose: true});
+let logWriter = rt.getStream({filename:"Logs/log", frequency:"daily", verbose: true});
 
 
 
